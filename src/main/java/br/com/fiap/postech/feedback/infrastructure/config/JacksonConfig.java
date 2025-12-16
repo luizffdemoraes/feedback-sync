beb17761d8b8b3a335a37e31294d2c510b4c2996
luizffdemoraes <lffm1994@gmail.com>
2025-12-16 01:25:30 -0300

    Refactor.

---
+++ src/main/java/br/com/fiap/postech/feedback/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package br.com.fiap.postech.feedback.infrastructure.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Singleton;
+
+@ApplicationScoped
+public class JacksonConfig {
+
+    @Produces
+    @Singleton
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+}
