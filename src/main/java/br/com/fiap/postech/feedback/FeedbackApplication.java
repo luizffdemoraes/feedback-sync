9140c9b725da138d80124c9fb8a7cb3cfa4463a1
luizffdemoraes <lffm1994@gmail.com>
2025-12-16 01:09:50 -0300

    Refactor.

--- src/main/java/br/com/fiap/postech/feedback/FeedbackApplication.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.postech.feedback;
-
-import io.quarkus.runtime.Quarkus;
-
-public class FeedbackApplication {
-    public static void main(String[] args) {
-        Quarkus.run(args);
-    }
-}
